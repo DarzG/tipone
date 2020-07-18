7b8ea30604d033fe3fba75c01278b5face7e7a14
Darryl Guillem <guillem.darryl.ph@gmail.com>
2020-07-18 13:30:18 +0800

    add frontend scripts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <!-- Styles -->
         <style>
             html, body {
@@ -91,7 +93,7 @@
 					<!-- Wrapper for slides -->
 					<div class="carousel-inner" role="listbox">
 					  <div class="item active">
-						<img src="ny.jpg" alt="New York" width="1200" height="700">
+						<img src="ny.jpg" alt="control" width="1200" height="700">
 						<div class="carousel-caption">
 						  <h3>Control</h3>
 						  <p></p>
@@ -99,7 +101,7 @@
 					  </div>
 
 					  <div class="item">
-						<img src="chicago.jpg" alt="Chicago" width="1200" height="700">
+						<img src="chicago.jpg" alt="manage" width="1200" height="700">
 						<div class="carousel-caption">
 						  <h3>Manage</h3>
 						  <p></p>
@@ -107,7 +109,7 @@
 					  </div>
 					
 					  <div class="item">
-						<img src="la.jpg" alt="Los Angeles" width="1200" height="700">
+						<img src="la.jpg" alt="operate" width="1200" height="700">
 						<div class="carousel-caption">
 						  <h3>Operate</h3>
 						  <p></p>
