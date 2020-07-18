f2649f77c1f00f070c8907f4f7069e30c965c057
Darryl Guillem <guillem.darryl.ph@gmail.com>
2020-07-18 19:42:16 +0800

    change frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,19 +21,15 @@
     </div-->
 	
 	<div class = "row" >
-		<div class="col-3" >
+		<div class="col-4" style="border-right: 2px solid #333">
 			<div>
 				<div><h1>Companies</h1></div>
 				
 			</div>
-			
-			
-		</div>
-		<div class="col-9 offset-2" >
 			<div class="form-group row">
-                            <label for="website" class="col-md-4 col-form-label text-md-right">{{ __('website') }}</label>
+                            <label for="website" class="col-md-4 col-form-label">{{ __('website') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="website" type="text" class="form-control @error('website') is-invalid @enderror" website="website" value="{{ old('website') }}" required autocomplete="website" autofocus>
 
                                 @error('website')
@@ -45,9 +41,9 @@
             </div>
 			
 			<div class="form-group row">
-                            <label for="company_email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="company_email" class="col-md-4 col-form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="company_email" type="company_email" class="form-control @error('company_email') is-invalid @enderror" website="company_email" value="{{ old('company_email') }}" required autocomplete="company_email">
 
                                 @error('company_email')
@@ -58,8 +54,8 @@
                             </div>
             </div>
 			<div class="row">
-				<label for="image" class="col-md-4 col-form-label text-md-right">logo</label>
-				<div class="col-md-6">
+				<label for="image" class="col-md-4 col-form-label ">logo</label>
+				<div class="col-md-5">
 					<input type="file", class="form-control-file" id="image" name="campany_logo">
 					@error('image')
                                     <span class="invalid-feedback" role="alert">
@@ -71,13 +67,11 @@
 			</div>
 			
 			<div class="form-group row">
-                            <label for="website" class="col-md-4 col-form-label text-md-right">{{ __('website') }}</label>
+                            <label for="website" class="col-md-4 col-form-label ">{{ __('website') }}</label>
 
                             <div class="col-md-6">
                                 <input id="website" type="text" class="form-control @error('website') is-invalid @enderror" website="website" value="{{ old('website') }}" required autocomplete="website" autofocus>
-									<div>
-										<button class="btn-primary">add company</button>
-									</div>
+									
 								@error('website')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -85,6 +79,18 @@
                                 @enderror
                             </div>
             </div>
+			
+			<div class="form-group row">
+				<button class="btn-primary">add company</button>
+			</div>
+			
+		</div>
+		<div class="col-8" >
+			<div><h4>company1</h4><div>
+			<div><h4>company2</h4><div>
+			<div><h4>company3</h4><div>
+			<div><h4>company4</h4><div>
+			<div><h4>company5</h4><div>
 			
 			
 		</div>
